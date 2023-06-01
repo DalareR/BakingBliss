f1363fc0f3914e3f59a72c3853bdaf582f0cf625
Dalare Rana <dalarerana@gmail.com>
2023-06-01 19:36:37 -0400

    add page animation to Hero & About compon

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -3,6 +3,41 @@ import { Grid, GridItem, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import aboutImg from "../assets/images/aboutImg.jpg";
 import { motion } from "framer-motion";
 
+const variantParent = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const variantChild = {
+  initial: {
+    opacity: 0,
+    y: "-30%",
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: "-30%",
+  },
+};
+
 export default function About() {
   return (
     <Grid
@@ -18,7 +53,13 @@ export default function About() {
       gridTemplateRows={{ base: "1fr 2fr", md: "auto" }}
       gridTemplateColumns={{ base: "auto", md: "2fr 1fr" }}
     >
-      <GridItem gridArea="img">
+      <GridItem
+        as={motion.div}
+        initial={{ x: "100%" }}
+        animate={{ x: "0", transition: { delay: 0.5, duration: 0.5 } }}
+        exit={{ x: "100%" }}
+        gridArea="img"
+      >
         <Image
           w="100%"
           h="100%"
@@ -30,11 +71,24 @@ export default function About() {
         />
       </GridItem>
       <GridItem gridArea="desc">
-        <VStack align="start">
-          <Heading fontWeight="400" textDecor="underline" mb={{ md: "3" }}>
+        <VStack
+          as={motion.div}
+          variants={variantParent}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          align="start"
+        >
+          <Heading
+            as={motion.div}
+            variants={variantChild}
+            fontWeight="400"
+            textDecor="underline"
+            mb={{ md: "3" }}
+          >
             About
           </Heading>
-          <Text>
+          <Text as={motion.div} variants={variantChild}>
             Welcome to <b>Baking Bliss</b>, a family-operated bakery located in
             the heart of Boston! We are passionate about baking delicious cakes,
             cupcakes, and pastries. Our recipes have been handed down from one
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,12 @@
-import { Flex, Grid, GridItem, Heading, Image, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  VStack,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
@@ -8,6 +16,41 @@ import gingerbread from "../assets/images/gingerbread.jpg";
 import poundCake from "../assets/images/poundCake.jpg";
 import lemonPie from "../assets/images/lemonPie.jpg";
 import Btn from "./Btn";
+
+const variantParent = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.05,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      when: "afterChildren",
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const variantChild = {
+  initial: {
+    opacity: 0,
+    y: "-30%",
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: "-30%",
+  },
+};
 
 export default function Hero() {
   const imgCarousel = [fruittart, gingerbread, poundCake, lemonPie];
@@ -19,7 +62,9 @@ export default function Hero() {
     const interval = setInterval(() => {
       setIndex((prev) => (prev === imgCarousel.length - 1 ? 0 : prev + 1));
     }, 10000);
-    return () => clearInterval(interval);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   window.addEventListener("resize", () => {
@@ -27,23 +72,27 @@ export default function Hero() {
   });
 
   return (
-    <AnimatePresence>
-      <Grid
-        as={motion.div}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { delay: 0.5 } }}
-        exit={{ opacity: 0 }}
-        w="100%"
-        h={`${winH - 50}px`}
-        bg="brand.50"
-        gridTemplateColumns={{ md: "1.5fr 1fr" }}
-        gridTemplateRows={{ base: "2fr 1fr", md: "auto" }}
-        gap="5"
-        alignItems="center"
-        overflow="hidden"
-        // outline="2px solid red"
-      >
-        <GridItem w="100%" h="100%" overflow="hidden">
+    <Grid
+      w="100%"
+      h={`${winH - 50}px`}
+      bg="brand.50"
+      gridTemplateColumns={{ md: "1.5fr 1fr" }}
+      gridTemplateRows={{ base: "2fr 1fr", md: "auto" }}
+      gap="5"
+      alignItems="center"
+      overflow="hidden"
+      // outline="2px solid red"
+    >
+      <AnimatePresence>
+        <GridItem
+          as={motion.div}
+          initial={{ x: "-100%" }}
+          animate={{ x: "0", transition: { delay: 0.5, duration: 0.5 } }}
+          exit={{ x: "-100%" }}
+          w="100%"
+          h="100%"
+          overflow="hidden"
+        >
           <Flex
             w="100%"
             h="100%"
@@ -65,21 +114,43 @@ export default function Hero() {
             ))}
           </Flex>
         </GridItem>
-        <GridItem>
-          <VStack align="start" p="2">
-            <Heading color="brand.500" size="2xl">
+      </AnimatePresence>
+      <GridItem>
+        <AnimatePresence>
+          <VStack
+            as={motion.div}
+            variants={variantParent}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            align="start"
+            p="2"
+          >
+            <Heading
+              as={motion.div}
+              variants={variantChild}
+              color="brand.500"
+              size="2xl"
+            >
               Eat Healthy Sweets
             </Heading>
-            <Heading size="lg" fontWeight="regular">
+            <Heading
+              as={motion.div}
+              variants={variantChild}
+              size="lg"
+              fontWeight="regular"
+            >
               We strive to provide delicious, freshly-baked treats for all
               occasions
             </Heading>
-            <Link to="/">
-              <Btn title="View Menu" colorScheme="brand" />
-            </Link>
+            <Box as={motion.div} variants={variantChild}>
+              <Link to="/">
+                <Btn title="View Menu" colorScheme="brand" />
+              </Link>
+            </Box>
           </VStack>
-        </GridItem>
-      </Grid>
-    </AnimatePresence>
+        </AnimatePresence>
+      </GridItem>
+    </Grid>
   );
 }
